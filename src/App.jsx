56ab053f0262630bfaee99d7a2a2170d824029f8
JP <jp@JPPC.localdomain>
2023-08-29 14:59:03 -0700

    uncommented SinglePlayer route and added NewPlayerForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import AllPlayers from './components/AllPlayers'
 import SinglePlayer from './components/SinglePlayer'
+import NewPlayerForm from './components/NewPlayerForm'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
@@ -13,8 +14,9 @@ function App() {
           <Navbar />
           <div id="main-section">
             <Routes>
-              <Route path='*' element={<AllPlayers />} />
-              {/* <Route path='/players/:id' element={<SinglePlayer />} /> */}
+              <Route path='/' element={<AllPlayers />} />
+              <Route path='/players/:id' element={<SinglePlayer />} />
+              <Route path='/newPlayerForm' element={<NewPlayerForm />} />
             </Routes>
           </div>
         </div>
