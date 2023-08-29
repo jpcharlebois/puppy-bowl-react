7991db0dbec39a61e0acf6f48a61f3c75ec68984
JP <jp@JPPC.localdomain>
2023-08-29 14:47:24 -0700

    added SinglePlayer component

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchSinglePlayer } from '../api/ajaxHelpers';
+import { useParams } from 'react-router-dom';
 
 export default function SinglePlayer() {
-    return (
-      <>
-        <p>This is a single player</p>
-      </>
-    )
+  const [player, setPlayer] = useState({});
+  let { id } = useParams();
+
+  function renderSinglePlayer() {
+    console.log("player: ", player);
+      return (
+        <div key={player.id} className="players" >
+          <h2>{player.name}</h2>
+          <h4>{player.breed}</h4>
+          <h4>{player.status}</h4>
+          <img src={player.imageUrl} />
+        </div>
+      )
+      }
+  
+  useEffect(() => {
+    async function singlePlayersHandler() {
+      const result = await fetchSinglePlayer(id);
+      console.log("result: ", result);
+      setPlayer(result);
+    }
+    singlePlayersHandler();
+  }, [])
+  
+  return (
+    <div>
+      {renderSinglePlayer()}
+    </div>
+  )
 }
